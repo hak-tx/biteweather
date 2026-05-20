b339bc8d6557cad282eb06b0c47b068c55563317
hak-tx <hakelbrian@gmail.com>
2026-05-19 19:50:17 -0500

    Fix iOS status bar layout

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,13 @@
 import { createRoot } from "react-dom/client";
+import { Capacitor } from "@capacitor/core";
+import { StatusBar, Style } from "@capacitor/status-bar";
 import App from "./App";
 import "./index.css";
+
+if (Capacitor.getPlatform() === "ios") {
+  document.documentElement.classList.add("capacitor-ios");
+  StatusBar.setOverlaysWebView({ overlay: false }).catch(() => {});
+  StatusBar.setStyle({ style: Style.Dark }).catch(() => {});
+}
 
 createRoot(document.getElementById("root")!).render(<App />);
